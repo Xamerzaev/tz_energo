cf70e2124f65af1907cc6d7034fea144464be35a
Mansur <mansur.ham@mail.ru>
2022-06-09 00:17:01 +0300

    a

--- core/routes.py
+++ core/routes.py
@@ -13,21 +13,19 @@ def task_1():
         c = form.c.data
         x_1 = 0
         x_2 = 0
-        if b**2+4*a*c == 0:
+        num = (b**2-4*a*c)**0.5
+        if num**(0.5) == 0:
             x_1 = -b/2*a
             result = x_1
             flash(f'Квадратное уравнение решено успешно! Ответ: {result}', 'success')
             return render_template('success.html')
-        elif b**2+4*a*c > 0:
-            num = b**2-4*a*c
-            num_2 = -b + num**(0.5)
-            x_1 = num_2/2*a
-            num_3 = -b - num**(0.5)
-            x_2 = num_3/2*a
+        elif num**(0.5) > 0:
+            x_1 = (-b + num) / (2*a)
+            x_2 = (-b - num) / (2*a)
             flash(f'Квадратное уравнение решено успешно! Ответ: 1-x = {x_1}, 2-x = {x_2}', 'success')
             return render_template('success.html')
 
-        elif b**2+4*a*c < 0:
+        elif num**(0.5) < 0:
             flash(f'Квадратное уравнение не имеет корней!', 'success') 
             return render_template('success.html')
 
@@ -40,5 +38,4 @@ def task_2():
 
 @app.route('/result')
 def result():
-
     return render_template('result.html')
